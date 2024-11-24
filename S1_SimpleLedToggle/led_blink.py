d66dfce683b9bfdae5e6de148a63fbfda32b5689
lb0016 <leventbilal.16@outlook.com>
2024-11-24 12:37:29 +0100

    fix2.1

--- S1_SimpleLedToggle/led_blink.py
+++ S1_SimpleLedToggle/led_blink.py
@@ -3,6 +3,7 @@ import time
 
 #   GPIO 4 will be used as LED project GPIO
 LED_PIN = 4
+PHY_PIN = 16
 
 GPIO.setmode(GPIO.BOARD)  #   Broadcom Chip Numbering
 GPIO.setup(LED_PIN, GPIO.OUT)
@@ -10,10 +11,10 @@ GPIO.setup(LED_PIN, GPIO.OUT)
 #   Toggling LED output to test the output
 try:
     while True:
-        GPIO.output(LED_PIN, GPIO.HIGH)
+        GPIO.output(PHY_PIN, GPIO.HIGH)
         print("LED is ON")
         time.sleep(1)
-        GPIO.output(LED_PIN,GPIO.LOW)
+        GPIO.output(PHY_PIN,GPIO.LOW)
         print("LED is OFF")
         time.sleep(1)
 except KeyboardInterrupt:
