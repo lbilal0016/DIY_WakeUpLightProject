dd1bfcc3e9a13d509d66d3a6c99c3c158f9c1ef2
lb0016 <leventbilal.16@outlook.com>
2024-11-24 18:39:15 +0100

    config and S2_PWM_Control added.

--- S1_SimpleLedToggle/led_blink.py
+++ S1_SimpleLedToggle/led_blink.py
@@ -1,20 +1,19 @@
 import RPi.GPIO as GPIO
 import time
-
-#   GPIO 4 will be used as LED project GPIO
-LED_PIN = 4
-PHY_PIN = 16
+import sys
+sys.path.append('/home/levent/Desktop/Project/DIY_WakeUpLightProject/config')
+import config
 
 GPIO.setmode(GPIO.BOARD)  #   Broadcom Chip Numbering
-GPIO.setup(PHY_PIN, GPIO.OUT)
+GPIO.setup(LED_PIN_BOARD, GPIO.OUT)
 
 #   Toggling LED output to test the output
 try:
     while True:
-        GPIO.output(PHY_PIN, GPIO.HIGH)
+        GPIO.output(LED_PIN_BOARD, GPIO.HIGH)
         print("LED is ON")
         time.sleep(1)
-        GPIO.output(PHY_PIN,GPIO.LOW)
+        GPIO.output(LED_PIN_BOARD,GPIO.LOW)
         print("LED is OFF")
         time.sleep(1)
 except KeyboardInterrupt:
