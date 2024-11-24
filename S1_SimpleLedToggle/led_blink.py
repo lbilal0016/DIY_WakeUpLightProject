3725460b0de920c2f3cee89250194da08ff39aae
lb0016 <leventbilal.16@outlook.com>
2024-11-24 12:32:28 +0100

    first test program debug lines added.

--- S1_SimpleLedToggle/led_blink.py
+++ S1_SimpleLedToggle/led_blink.py
@@ -11,8 +11,10 @@ GPIO.setup(LED_PIN, GPIO.OUT)
 try:
     while True:
         GPIO.output(LED_PIN, GPIO.HIGH)
+        print("LED is ON")
         time.sleep(1)
         GPIO.output(LED_PIN,GPIO.LOW)
+        print("LED is OFF")
         time.sleep(1)
 except KeyboardInterrupt:
     print("Exiting test program...")
