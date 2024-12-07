e4a2153f063f0e3dcf866575b14579f6212a4b4e
lb0016 <leventbilal.16@outlook.com>
2024-12-07 20:22:45 +0100

    some corrections in wakeUpTimeHourAndMinute class

--- 10_Libraries/TimeLibrary.py
+++ 10_Libraries/TimeLibrary.py
@@ -14,8 +14,7 @@ class wakeupTimeHourAndMinute():
         if wakeUpHour is None and wakeUpMinute is None:
             self._wakeUpHour = 0
             self._wakeUpMinute = 0
-            self._returnHourMinute.minute = 0
-
+            
         elif isinstance(wakeUpHour, int) and isinstance(wakeUpMinute, int):
             if wakeUpHour < HOURS_IN_DAY or wakeUpMinute < MINUTES_IN_HOUR:
                 self._wakeUpHour = wakeUpHour
@@ -42,26 +41,21 @@ class wakeupTimeHourAndMinute():
         else:
             raise ValueError("Invalid arguments. Hour and minute arguments should be integer values")
     
-    def timeDiff(self, hourInput, minuteInput):
-        if not (isinstance(hourInput, int) and isinstance(minuteInput, int)):
-            raise ValueError("Invalid arguments. Hour and minute arguments should be integer values")
-        if not (0 <= hourInput < HOURS_IN_DAY and 0 <= minuteInput < MINUTES_IN_HOUR):
-            raise ValueError("Invalid arguments. Hour must be [0 - 23] and minute must be [0 - 59]")  
-        
+    def timeDiff(self):
         systemTimeMin = time.localtime(time.time()).tm_min
         systemTimeHour = time.localtime(time.time()).tm_hour
     
-        if systemTimeHour == hourInput:
-            if not minuteInput < systemTimeMin:
-                return (0, minuteInput - systemTimeMin)
+        if systemTimeHour == self._wakeUpHour:
+            if not self._wakeUpMinute < systemTimeMin:
+                return (0, self._wakeUpMinute - systemTimeMin)
             else:
                 #   wakeUpTime has passed
                 return (0,0)
-        elif hourInput < systemTimeHour:
+        elif self._wakeUpHour < systemTimeHour:
             #   wakeUpTime has passed
             return (0,0)
         else:
-            if minuteInput < systemTimeMin:
+            if self._wakeUpMinute < systemTimeMin:
                 """
                 Example use case:
                 system time: 19:50
@@ -70,7 +64,7 @@ class wakeupTimeHourAndMinute():
                 Hour: (userInputHour - systemTimeHour) - 1
                 Minute: (MINUTES_IN_HOUR - systemTimeMinute) + userInputMinute
                 """
-                return(hourInput - systemTimeHour - 1, (MINUTES_IN_HOUR - systemTimeMin) + minuteInput)
+                return(self._wakeUpHour - systemTimeHour - 1, (MINUTES_IN_HOUR - systemTimeMin) + self._wakeUpMinute)
             else:
                 """
                 Example use case:
@@ -80,5 +74,5 @@ class wakeupTimeHourAndMinute():
                 Hour: (userInputHour - systemTimeHour)
                 Minute: (userInputMinute - systemTimeMinute)
                 """
-                return (hourInput - systemTimeHour, minuteInput - systemTimeMin)
+                return (self._wakeUpHour - systemTimeHour, self._wakeUpMinute - systemTimeMin)
 
