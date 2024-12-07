e847b25d7e70485aad38a6b907bbfdfd6b5a6fb7
lb0016 <leventbilal.16@outlook.com>
2024-12-07 21:16:47 +0100

    bugfix implemented and successfully tested.

--- S3_TimeTriggeredPWMControl/main.py
+++ S3_TimeTriggeredPWMControl/main.py
@@ -23,6 +23,7 @@ def main():
                             alarmClock.resetWakeUpTime(wakeUpHour, wakeUpMinute)
                             print(f"Wake-up time set to: {wakeUpHour:02d}:{wakeUpMinute:02d}")
                         #   Else case is already handled in checkTimeValidity method
+                        
                 except (ValueError, SyntaxError):
                     print("Error: Invalid input format. Please enter a tuple in the format (hh, mm) or enter q to exit.")
                 
